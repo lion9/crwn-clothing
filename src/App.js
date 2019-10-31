97fee8210b59ac206ec913ab1864ce9bfa1437c7
Artyom <a.vecherov@gmail.com>
2019-11-01 00:12:29 +0300

    feat: add shop page and relevant components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import './App.css';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <HomePage />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch> 
+    </BrowserRouter>
   );
 }
 
